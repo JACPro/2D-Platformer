417f0c5d81d0d933f1789f69a0f1b6a392f26ab9
JCunningham <jamesacpro@gmail.com>
2025-01-04 22:22:05 +0000

    Create IMC and Input actions, instantiate in PlayerCharacter
    Signed-off-by: JCunningham <jamesacpro@gmail.com>

--- Source/CrustyPirate/PlayerCharacter.cpp
+++ Source/CrustyPirate/PlayerCharacter.cpp
@@ -15,9 +15,44 @@ APlayerCharacter::APlayerCharacter()
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = 
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
 }
 	
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::JumpStarted(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::JumpEnded(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::Attack(const FInputActionValue& Value)
+{
 }
--- Source/CrustyPirate/PlayerCharacter.h
+++ Source/CrustyPirate/PlayerCharacter.h
@@ -6,6 +6,12 @@
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/InputComponent.h"
+#include "InputActionValue.h"
+#include "GameFramework/Controller.h"
+
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -20,7 +26,28 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UCameraComponent* Camera;
 
+
+	// Input
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* JumpAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* AttackAction;
+
+	
 	APlayerCharacter();
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void Move(const FInputActionValue& Value);
+	void JumpStarted(const FInputActionValue& Value);
+	void JumpEnded(const FInputActionValue& Value);
+	void Attack(const FInputActionValue& Value);
 };
