fbdea0ebf2ae557312d73da12248fbde9dc98692
JCunningham <jamesacpro@gmail.com>
2025-01-05 16:11:28 +0000

    Enemy detect if player within sphere and move towards player
    Signed-off-by: JCunningham <jamesacpro@gmail.com>

--- Source/CrustyPirate/Enemy.cpp
+++ Source/CrustyPirate/Enemy.cpp
@@ -19,14 +19,33 @@ void AEnemy::BeginPlay()
 void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (IsAlive && FollowTarget)
+	{
+		float MoveDirection = (FollowTarget->GetActorLocation().X - GetActorLocation().X) > 0.0f ? 1.0f : -1.0f;
+
+		if (CanMove)
+		{
+			FVector WorldDirection = FVector::ForwardVector;
+			AddMovementInput(WorldDirection, MoveDirection);
+		}
+	}
 }
 
 void AEnemy::DetectorOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor))
+	{
+		FollowTarget = Player;
+	}
 }
 
 void AEnemy::DetectorOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
+	if (APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor))
+	{
+		FollowTarget = NULL;
+	}
 }
