6b1cb8f664b31457f7221fac93d444d5157a58a6
JCunningham <jamesacpro@gmail.com>
2025-01-11 23:47:33 +0000

    Upload windows build zip to LFS, remove debug statement on change levels
    Signed-off-by: JCunningham <jamesacpro@gmail.com>

--- Source/CrustyPirate/CrustyPirateGameInstance.cpp
+++ Source/CrustyPirate/CrustyPirateGameInstance.cpp
@@ -19,7 +19,6 @@ void UCrustyPirateGameInstance::ChangeLevel(int LevelIndex)
 	CurrentLevelIndex = LevelIndex;
 
 	FString LevelNameString = FString::Printf(TEXT("Level_%02d"), CurrentLevelIndex);
-	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, LevelNameString);
 	UGameplayStatics::OpenLevel(GetWorld(), FName(LevelNameString));
 }
 
